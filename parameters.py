f16ed4a03736c62e263ec29ae2636a79d396e38e
GabyRkt <80094788+GabyRkt@users.noreply.github.com>
2023-02-24 14:23:00 +0100

    update parameters comment

--- parameters.py
+++ parameters.py
@@ -1,47 +1,60 @@
-
-"""
-This class contains parameters used for an agent's training .
-"""
-
-
 class Parameters :
+  """
+  This class contains settings used for an agent's training.
+  """
 
   def __init__(self) :
     
-    self.MAX_N_STEPS = int(1e5)
-    self.MAX_N_EPISODES = 50
+    self.MAX_N_STEPS = int(1e5)       # maximum number of steps in a simulation
+    self.MAX_N_EPISODES = 50          # maximum number of episodes in a simulation
+
 
     #params for pre-exploration :
 
-    self.preExplore = True  
-    self.Tgoal2start = True   
+    self.preExplore = True            # let the agent explore the maze without rewards before the first episode
+    self.Tgoal2start = True           # include a transition from goal to start in transition matrix
+
 
     #params for action selection policies :
 
-    self.actpolicy = "softmax"    # "softmax" or else "egreedy"
-    self.epsilon = 0.1    # exploration rate for egreedy
-    self.tau = 1/5    # inverse temperature for softmax
+    self.actpolicy = "softmax"        
+    "softmax or egreedy"
+
+    self.epsilon = 0.1                
+    "exploration rate for egreedy"
+
+    self.tau = 1/5                    
+    "inverse temperature for softmax"
+
 
     #params for Q-learning :
 
-    self.gamma = 0.9    # discount rate
-    self.alpha = 1.0    # learning rate
-    self.lmbda = 0.1    # eligibility trace's decay rate
+    self.gamma = 0.9
+    "discount rate"    
+    
+    self.alpha = 1.0  
+    "learning rate"  
+    
+    self.lmbda = 0.1
+    "eligibility trace's decay rate"
+
 
     #params for Transition Matrix :
     
-    self.Talpha = 0.9
+    self.Talpha = 0.9                 
+    "learning rate for the transition matrix"
 
-    #params for planning
 
-    self.planOnlyAtGorS = True
-    self.Nplan = 20
-    self.remove_samestate = True
-    self.expandFurther = True
-    self.onlineVSoffline = "online"    # "online" = Successor Representation
-    self.allowLoops = False    # allow loops in n-step backups
-    self.baselineGain = int(1e10)
-    self.EVBthresh = 0
+    #params for planning:
+
+    self.planOnlyAtGorS = True        # indicates if the planning should only happen if the agent is at the start or goal state
+    self.Nplan = 20                   # number of steps in a planning
+    self.remove_samestate = True      # remove actions that lead to the same state (i.e. hitting a wall)
+    self.expandFurther = True         # expand the last backup further
+    self.onlineVSoffline = "online"   # "online" = Successor Representation
+    self.allowLoops = False           # allow loops in n-step backups
+    self.baselineGain = int(1e10)     # gain is set to at least this value
+    self.EVBthresh = 0                # minimum EVB so that planning is performed
 
 
 
