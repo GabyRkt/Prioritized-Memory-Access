43ee44ef5dea6d286ea0f97714bd9afc4776d504
Hieu LE <118977374+Hieu-L@users.noreply.github.com>
2023-04-11 12:40:54 +0200

    changed the y-axis and colours
    y-axis : now shows the difference in percentage , instead of a fraction
    
    colors : changed from uniform blue to a PLEASANT

--- fig5-ce.py
+++ fig5-ce.py
@@ -16,6 +16,10 @@ def plot_fig5ce( nb_sims : int = 100 ) :
     p.epsilon = 0.1
     p.start_rand = False
     p.Tgoal2start = True
+    p.onlineVSoffline = "online"
+    
+    # starting a plot, divided into 4 quadrants
+    figure, axis = plt.subplots(2, 2)
 
     # variables to plot
     x1_forward = 1        # number of forward events when r isnt changed (r = 1)
@@ -28,6 +32,7 @@ def plot_fig5ce( nb_sims : int = 100 ) :
     x0_backward = 1      # number of forwards after a drop in r (r = 0)
 
 
+    
     # finding the number of forwards & backwards when r = 4 every other for 25/50 episodes
     p.changeR = True ; p.x4 = True ; p.x0 = False
 
@@ -35,31 +40,32 @@ def plot_fig5ce( nb_sims : int = 100 ) :
         print("[fig 5c] : running simulation : "+str(i+1)+"/"+str(nb_sims))
         log = run_simulation(m,p)
 
-        x1_forward += sum( [ log.forward[k1] for k1 in range(50) if k1 not in [2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31,34,35,38,39,42,43,46,47] ] )
-        x4_forward += sum( [ log.forward[k1] for k1 in [2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31,34,35,38,39,42,43,46,47] ] )
+        x1_forward += sum( [ log.forward[k1] for k1 in range(50) if k1 not in log.event_episode ] )
+        x4_forward += sum( [ log.forward[k1] for k1 in log.event_episode ] )
 
-        x1_backward += sum( [ log.backward[k1] for k1 in range(50) if k1 not in [2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31,34,35,38,39,42,43,46,47] ] )
-        x4_backward += sum( [ log.backward[k1] for k1 in [2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31,34,35,38,39,42,43,46,47] ] )
+        x1_backward += sum( [ log.backward[k1] for k1 in range(50) if k1 not in log.event_episode ] )
+        x4_backward += sum( [ log.backward[k1] for k1 in log.event_episode ] )
 
     x = [ "1x/1x" , "4x/1x" ]
 
-    y_forward = [ x1_forward - x1_forward , (x4_forward - x1_forward)/x1_forward]
-    y_backward = [ x1_backward - x1_backward , (x4_backward - x1_backward)/x1_backward]
-
-    figure, axis = plt.subplots(2, 2)
+    # calculate the difference 
+    y_forward = [ 100*(x1_forward - x1_forward)/x1_forward , 100*(x4_forward - x1_forward)/x1_forward]
+    y_backward = [ 100*(x1_backward - x1_backward)/x1_backward , 100*(x4_backward - x1_backward)/x1_backward]
 
     # fig 5 c : forward
-    axis[0,0].bar(x,y_forward)
+    axis[0,0].bar(x,y_forward,color = ["grey","black"])
     axis[0,0].set_title("Forward after increase in r (1->4)")
     axis[0,0].axhline(y=0,linewidth=1, color='k')
-    axis[0,0].set_ylim([-5,5])
+    axis[0,0].set_ylim([-100,1000])
 
     # fig 5 c : backward
-    axis[0,1].bar(x,y_backward)
+    axis[0,1].bar(x,y_backward, color = ["lightblue","blue"])
     axis[0,1].set_title("Backward after increase in r (1->4)")
     axis[0,1].axhline(y=0,linewidth=1, color='k')
-    axis[0,1].set_ylim([-5,5])
+    axis[0,1].set_ylim([-100,1000])
 
+
+    
     # finding the number of forwards & backwards when r = 0 every other for 25/50 episodes
     p.changeR = True ; p.x4 = False ; p.x0 = True
 
@@ -67,31 +73,33 @@ def plot_fig5ce( nb_sims : int = 100 ) :
         print("[fig 5e] : running simulation (2/2) : "+str(i+1)+"/"+str(nb_sims))
         log = run_simulation(m,p)
 
-        x1_forward += sum( [ log.forward[k1] for k1 in range(50) if k1 not in [2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31,34,35,38,39,42,43,46,47] ] )
-        x0_forward += sum( [ log.forward[k1] for k1 in [2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31,34,35,38,39,42,43,46,47] ] )
+        x1_forward += sum( [ log.forward[k1] for k1 in range(50) if k1 not in log.event_episode ] )
+        x0_forward += sum( [ log.forward[k1] for k1 in log.event_episode] )
 
-        x1_backward += sum( [ log.backward[k1] for k1 in range(50) if k1 not in [2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31,34,35,38,39,42,43,46,47] ] )
-        x0_backward += sum( [ log.backward[k1] for k1 in [2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31,34,35,38,39,42,43,46,47] ] )
-
+        x1_backward += sum( [ log.backward[k1] for k1 in range(50) if k1 not in log.event_episode ] )
+        x0_backward += sum( [ log.backward[k1] for k1 in log.event_episode ] )
+    
+    
+    # calculate the difference 
     x = [ "1x/1x" , "0x/1x" ]
 
-    y_forward = [ x1_forward - x1_forward , (x0_forward - x1_forward)/x1_forward]
-    y_backward = [ x1_backward - x1_backward , (x0_backward - x1_backward)/x1_backward]
+    y_forward = [ 100*(x1_forward - x1_forward)/x1_forward  , 100*(x0_forward - x1_forward)/x1_forward]
+    y_backward = [ 100*(x1_backward - x1_backward)/x1_backward , 100*(x0_backward - x1_backward)/x1_backward]
 
     # fig 5 e : forward
-    axis[1,0].bar(x,y_forward)
+    axis[1,0].bar(x,y_forward, color = ["grey","black"] )
     axis[1,0].set_title("Forward after drop in r (1->0)")
     axis[1,0].axhline(y=0,linewidth=1, color='k')
-    axis[1,0].set_ylim([-5,5])
+    axis[1,0].set_ylim([-100,100])
 
     # fig 5 e : backward
-    axis[1,1].bar(x,y_backward)
-    axis[1,1].set_title("Forward after drop in r (1->0)")
+    axis[1,1].bar(x,y_backward, color = ["lightblue","blue"])
+    axis[1,1].set_title("Backward after drop in r (1->0)")
     axis[1,1].axhline(y=0,linewidth=1, color='k')
-    axis[1,1].set_ylim([-5,5])
+    axis[1,1].set_ylim([-100,100])
 
     plt.show()
 
     return
 
-plot_fig5ce(1)
+plot_fig5ce(50)
