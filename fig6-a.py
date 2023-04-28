03ad7e3e1089bdbc2d903514816fd75024997229
Hieu LE <118977374+Hieu-L@users.noreply.github.com>
2023-04-28 20:40:50 +0200

    extended this fig to represent all backups
    added a 4th figure that displays the total amount of Bellman backups made throughout each episode, not just the ones made during planning.

--- fig6-a.py
+++ fig6-a.py
@@ -1,11 +1,12 @@
-from maze import LinearTrack, OpenField
-from parameters import Parameters
+from prio_replay.maze import LinearTrack, OpenField
+from prio_replay.parameters import Parameters
 import numpy as np
-from simulation import run_simulation
+from prio_replay.simulation import run_simulation
 import matplotlib.pyplot as plt
 
 import warnings
 warnings.filterwarnings("ignore")
+
 
 def plot_fig6a( nb_sims : int = 100 ) :
 
@@ -15,7 +16,7 @@ def plot_fig6a( nb_sims : int = 100 ) :
     p.actpolicy = "softmax"
     p.tau = 0.2
     p.epsilon = 0.1
-    p.sigma = 0.5
+    p.sigma = 0.1
     p.start_rand = False
     p.Tgoal2start = True
     p.onlineVSoffline = "online"
@@ -25,6 +26,7 @@ def plot_fig6a( nb_sims : int = 100 ) :
     # variables to store and plot data
     forward = [0] * 50
     backward = [0] * 50
+    activation = [0] * 50
 
     for i in range(nb_sims) :
         print("[fig 6a] : running simulation : "+str(i+1)+"/"+str(nb_sims))
@@ -33,31 +35,33 @@ def plot_fig6a( nb_sims : int = 100 ) :
 
         # get the average amount of forward or backward per episode
         forward = [ forward[k] + (log.forward[k]/nb_sims) for k in range(50) ]
-        backward = [ backward[k] + (log.backward[k]/nb_sims) for k in range(50) ]
-    
+        backward = [ backward[k] + (log.backward[k]/nb_sims) for k in range(50) ]   
+        activation = [ activation[k] + (log.nb_replay_per_ep[k]/nb_sims) for k in range(50)]
 
     # calculate the average between forward and backward
     avg_f_b = [ (f + b) / 2 for f, b in zip(forward, backward) ]
 
-
-
-    figure, axis = plt.subplots(1, 3)
+    print(activation)
+    figure, axis = plt.subplots(2, 2)
 
     # plot : FORWARD EVENTS PER EPISODE
-    axis[0].plot(forward)
-    axis[0].set_title("forward events per episode")
+    axis[0][0].plot(forward)
+    axis[0][0].set_title("forward events per episode")
 
     # plot : BACKWARD EVENTS PER EPISODE
-    axis[1].plot(backward)
-    axis[1].set_title("backward events per episode")
+    axis[0][1].plot(backward)
+    axis[0][1].set_title("backward events per episode")
 
     # plot : REPLAY EVENTS (AVERAGE BETWEEN BACKWARD AND FORWARD) PER EPISODE
-    axis[2].plot(avg_f_b)
-    axis[2].set_title("replay events per episode")
+    axis[1][0].plot(avg_f_b)
+    axis[1][0].set_title("average Backward or Forward per episode")
+
+    axis[1][1].plot(activation)
+    axis[1][1].set_title("average activation probability per episode")
 
 
     plt.show()
 
     return
 
-plot_fig6a(50)
+plot_fig6a(25)
